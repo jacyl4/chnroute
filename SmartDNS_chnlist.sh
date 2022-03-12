1b47e03bb8e85be0f658c6173fd9feabbe401c28
JacyL4 <jacyl4@gmail.com>
2022-03-12 19:17:11 +0800

    Update SmartDNS_chnlist.sh

--- SmartDNS_chnlist.sh
+++ SmartDNS_chnlist.sh
@@ -3,12 +3,10 @@ mkdir -p ./SmartDNS_chnlist
 sudo rm -rf ./SmartDNS_chnlist/*
 wget --show-progress -cqO- https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf | sed 's/server=/nameserver /g' | rev | cut -d / -f2- | rev | sed 's?$?/chnDNS?g'>./SmartDNS_chnlist/accelerated-domains.china.conf
 wget --show-progress -cqO- https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf | sed 's/server=/nameserver /g' | rev | cut -d / -f2- | rev | sed 's?$?/chnDNS?g'>./SmartDNS_chnlist/apple.china.conf
-wget --show-progress -cqO- https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf | sed 's/server=/nameserver /g' | rev | cut -d / -f2- | rev | sed 's?$?/chnDNS?g'>./SmartDNS_chnlist/google.china.conf
 
 cp -f IPchnroute ./SmartDNS_chnlist/whitelist-ip-chnlist.conf
 sed -i 's/^/whitelist-ip /g' ./SmartDNS_chnlist/whitelist-ip-chnlist.conf
 
 sha256sum ./SmartDNS_chnlist/accelerated-domains.china.conf | awk '{print$1}' >./SmartDNS_chnlist/accelerated-domains.china.conf.sha256sum
 sha256sum ./SmartDNS_chnlist/apple.china.conf | awk '{print$1}' >./SmartDNS_chnlist/apple.china.conf.sha256sum
-sha256sum ./SmartDNS_chnlist/google.china.conf | awk '{print$1}' >./SmartDNS_chnlist/google.china.conf.sha256sum
 sha256sum ./SmartDNS_chnlist/whitelist-ip-chnlist.conf | awk '{print$1}' >./SmartDNS_chnlist/whitelist-ip-chnlist.conf.sha256sum
