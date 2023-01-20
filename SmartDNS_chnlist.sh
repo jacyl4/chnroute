48a484495cf7b40f39091e4055a8551348174b9b
JacyL4 <jacyl4@gmail.com>
2023-01-20 23:19:19 +0800

    Update SmartDNS_chnlist.sh

--- SmartDNS_chnlist.sh
+++ SmartDNS_chnlist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p ./SmartDNS_chnlist
 sudo rm -rf ./SmartDNS_chnlist/*
-wget --show-progress -cqO- https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf | sed 's/server=/nameserver /g' | rev | cut -d / -f2- | rev | sed 's?$?/chnDNS?g'>./SmartDNS_chnlist/accelerated-domains.china.conf
-wget --show-progress -cqO- https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf | sed 's/server=/nameserver /g' | rev | cut -d / -f2- | rev | sed 's?$?/chnDNS?g'>./SmartDNS_chnlist/apple.china.conf
+wget --show-progress -cqO- https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf | sed 's/server=/nameserver /g' | rev | cut -d / -f2- | rev | sed 's?$?/DNS_chn?g'>./SmartDNS_chnlist/accelerated-domains.china.conf
+wget --show-progress -cqO- https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf | sed 's/server=/nameserver /g' | rev | cut -d / -f2- | rev | sed 's?$?/DNS_chn?g'>./SmartDNS_chnlist/apple.china.conf
 wget --show-progress -cqO- https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/bogus-nxdomain.china.conf | sed 's/bogus-nxdomain=/bogus-nxdomain /g' >./SmartDNS_chnlist/bogus-nxdomain.china.conf
 
 cp -f IPchnroute ./SmartDNS_chnlist/whitelist-ip-chnlist.conf
